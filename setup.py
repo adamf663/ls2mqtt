4edd6852fc85774dd6660e821d690d5dedb62a3a
Adam Felson <adam.github@thefelsons.us>
2021-12-09 06:59:54 -0700

    Initial version.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ls2mqtt',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='adam',
+    author_email='adam.github@thefelsons.us',
+    description='An mqtt gateway for sensible living sensors'
+)
